56a0c91c35f90d7341e07e73bf108bf2b22b1022
Gekif <dzulfikar.maulana@gmail.com>
2020-03-11 23:37:20 +0700

    2. The Ruby Programming Language: 2. Introduction to Section 2 and Ruby

---
+++ ruby_project/playground.rb
@@ -0,0 +1,23 @@
+# Say hello world in multiple ways
+
+# Simple put string, puts enter one line
+puts "Hello World using puts"
+
+
+# Using a variable, Print not enter one line
+greeting = "Hello World using print\n"
+
+print greeting
+
+
+# Using a method
+def say_hello(name)
+  puts "Hello World #{name} using method"
+end
+
+say_hello("Fikar")
+
+
+#Using p
+p "Hello world using p for double quote"
+
