2527dbe8d9e61fc11b10711def4f35439d7e3ad4
Gekif <dzulfikar.maulana@gmail.com>
2020-03-15 08:59:42 +0700

    2. The Ruby Programming Language: 5. Working with Strings part 2 Getting input from user

---
+++ ruby_project/input.rb
@@ -0,0 +1,12 @@
+# puts "what is your first name?"
+
+# first_name = gets.chomp
+
+# puts "Your first name is #{first_name.capitalize}"
+
+
+puts "Enter a number to multiply by 2:"
+
+input = gets.chomp
+
+puts input.to_i * 2
