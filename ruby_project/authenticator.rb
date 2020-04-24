02341b43be61ee4ab6d7cf7f1fde1092f2c1cdf6
Gekif <dzulfikar.maulana@gmail.com>
2020-04-24 22:06:52 +0700

    2. The Ruby Programming Language: 20. Authenticator project implementation

--- ruby_project/authenticator.rb
+++ ruby_project/authenticator.rb
@@ -1,10 +1,22 @@
 users = [
-    { username: 'fikar', password: 'password1' },
+    { username: 'fikar', password: 'fikar' },
     { username: 'john', password: 'john' },
     { username: 'joe', password: 'joe' },
     { username: 'doe', password: 'doe' },
     { username: 'mary', password: 'mary' },
 ]
+
+
+def auth_user(username, password, list_of_users)
+  list_of_users.each do |user_record|
+    if user_record[:username] == username && user_record[:password] == password
+      return user_record
+    end
+  end
+
+  'Credentials were not correct'
+end
+
 
 puts 'Welcome to the authenticator'
 25.times { print '-' }
@@ -12,5 +24,22 @@ puts
 puts 'This program will take input from the user and compare password'
 puts 'If password is correct, you will get back the user object'
 
-# We will learn
-# Hash, Array, Branching, While loops and designing program execution flow
+
+attempts = 1
+
+while attempts < 4
+  print 'Username: '
+  username = gets.chomp
+
+  print 'Password: '
+  password = gets.chomp
+
+  puts auth_user(username, password, users)
+
+  puts 'Press n to quit or any other key to continue: '
+  input = gets.chomp.downcase
+  break if input == 'n'
+  attempts += 1
+end
+
+puts 'You have exceeded the number of attempts' if attempts == 4
