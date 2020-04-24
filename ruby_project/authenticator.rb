f557bb3099894240d106794822939986ed23104c
Gekif <dzulfikar.maulana@gmail.com>
2020-04-24 21:45:46 +0700

    2. The Ruby Programming Language: 19. Homework Project Authenticator

---
+++ ruby_project/authenticator.rb
@@ -0,0 +1,16 @@
+users = [
+    { username: 'fikar', password: 'password1' },
+    { username: 'john', password: 'john' },
+    { username: 'joe', password: 'joe' },
+    { username: 'doe', password: 'doe' },
+    { username: 'mary', password: 'mary' },
+]
+
+puts 'Welcome to the authenticator'
+25.times { print '-' }
+puts
+puts 'This program will take input from the user and compare password'
+puts 'If password is correct, you will get back the user object'
+
+# We will learn
+# Hash, Array, Branching, While loops and designing program execution flow
