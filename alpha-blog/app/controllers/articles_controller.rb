c3a29f3f08fb9fbae50f4d29b089a9824c76078d
Gekif <dzulfikar.maulana@gmail.com>
2020-05-05 17:00:16 +0700

    4. CRUD Operations in Ruby on Rails: 16. Deploy and Wrap Section 4

--- alpha-blog/app/controllers/articles_controller.rb
+++ alpha-blog/app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:edit, :update, :show, :destroy]
+
   def new
     @article = Article.new
   end
@@ -20,18 +22,14 @@ class ArticlesController < ApplicationController
 
 
   def show
-    @article = Article.find(params[:id])
   end
 
 
   def edit
-    @article = Article.find(params[:id])
   end
 
 
   def update
-    @article = Article.find(params[:id])
-
     if @article.update(article_params)
       flash[:notice] = "Article was successfully updated"
 
@@ -48,8 +46,6 @@ class ArticlesController < ApplicationController
 
 
   def destroy
-    @article = Article.find(params[:id])
-
     @article.destroy
 
     flash[:notice] = "Article was successfully deleted"
@@ -59,6 +55,11 @@ class ArticlesController < ApplicationController
 
 
   private
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+
   def article_params
     params.require(:article).permit(:title, :description)
   end
